524c137f0f1e754cf29549069d3226574f932945
Adria <adria.marata-fernandez@epitech.eu>
2021-04-27 12:11:05 +0200

    ball firing finished but with codingstyle

--- src/game/ball_moves.c
+++ src/game/ball_moves.c
@@ -10,25 +10,29 @@
 int ball_moves(sfEvent event, game_t *game, int a)
 {
     if (game->ball->pos.x < 0 || game->ball->pos.x > 1920
-    || game->ball->pos.y < 0 || game->ball->pos.y > 1080) {
+    || game->ball->pos.y < 0 || game->ball->pos.y > 1080 || (a == 0 &&
+    (game->ball->pos.x >= game->player->elem->pos.x - 20
+    && game->ball->pos.x <= game->player->elem->pos.x + 20
+    && game->ball->pos.y >= game->player->elem->pos.y - 20
+    && game->ball->pos.y <= game->player->elem->pos.y + 20))) {
         game->ball->pos.x = game->player->elem->pos.x;
         game->ball->pos.y = game->player->elem->pos.y;
         return (0);
     }
     if (a == 1 || game->ball->pos.x < game->player->elem->pos.x) {
-        game->ball->pos.x += -30;
+        game->ball->pos.x += -40;
         return (0);
     }
     if (a == 2 || game->ball->pos.y < game->player->elem->pos.y) {
-        game->ball->pos.y += -30;
+        game->ball->pos.y += -40;
         return (0);
     }
     if (a == 3 || game->ball->pos.y > game->player->elem->pos.y) {
-        game->ball->pos.y += 30;
+        game->ball->pos.y += 40;
         return (0);
     }
     if (a == 4 || game->ball->pos.x > game->player->elem->pos.x) {
-        game->ball->pos.x += 30;
+        game->ball->pos.x += 40;
         return (0);
     }
     return (0);
